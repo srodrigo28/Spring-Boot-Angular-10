cf5d1e63375d1ff4899166cf5a204cbf5c1bfb8a
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-07-15 19:22:45 -0300

    Configurado o Banco H2

--- src/main/java/com/bootstrap/BootstrapApplication.java
+++ src/main/java/com/bootstrap/BootstrapApplication.java
@@ -1,13 +1,20 @@
 package com.bootstrap;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootstrapApplication {
+public class BootstrapApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootstrapApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
